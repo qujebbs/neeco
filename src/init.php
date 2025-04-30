c334a22fcd98258bf4e9d1fd213e86a25b75b3e4
qujebbs <markj00767@gmail.com>
2025-04-30 10:57:33 +0800

    pre tests

--- src/init.php
+++ src/init.php
@@ -1,9 +1,2 @@
 <?php
 require_once __DIR__ . "/../src/config/db.php";
-require_once __DIR__ . "/../src/utils/qrylogic.php"; 
-require_once __DIR__ . "/../src/utils/strip.php";
-require_once __DIR__ . "/../src/utils/escapeoutput.php";
-
-
-
-
